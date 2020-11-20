e0f58bd8e8165fe7b3a3a7c0333a06315fb95cb7
Ali <a.mouebo@igfca.org>
2020-11-20 17:30:10 +0100

    Ajout de ma première contribution (Ali)

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
           
           Edit <code>src/App.js</code> and save to reload.
         </p>
+		<p>
+			Voici la contribution de Ali
+		</p>
         <a
           className="App-link"
           href="https://reactjs.org"
