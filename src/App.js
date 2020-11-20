3956c31736c12d3dd1d2b2d222f5f36b593bf296
emmyxeme <emmyxeme@gmail.com>
2020-11-20 17:30:16 +0100

    ajout de la contribution Ardi

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          
+        Voici la contribution de Ardi depuis le compte adminIT
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
